8a062267db3e05cbb5cbb8e4a2f5a98bfb3c478a
Cristhian Casierra <ccasierra88@hotmail.com>
2021-10-09 09:34:26 +0200

    .styles pagefiles deleted and moved to mainpage

--- src/pages/projets/index.js
+++ src/pages/projets/index.js
@@ -2,7 +2,33 @@ import { Link, graphql } from 'gatsby'
 import * as React from 'react'
 import Card from '../../components/Card'
 import Layout from "../../components/layout"
-import { Wrapper, Grid } from './index.style'
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+width: 80vw;
+margin: 20px 0;
+
+    h1 {
+        
+    }
+`
+
+const Grid = styled.div`
+    width: 80vw;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    align-self: center;
+
+    @media screen and (max-width: 700px) {
+
+    grid-template-columns: 1fr;
+    width: 60vw;
+    gap: 20px;
+}
+`
 
 const ProjetsPage = ({ data }) => {
 
--- src/pages/projets/{contentfulProject.slug}.js
+++ src/pages/projets/{contentfulProject.slug}.js
@@ -1,9 +1,106 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import { graphql, Link } from 'gatsby'
-import { Body, Title, Wrapper } from './{contentfulProject.slug}.style'
 import { ArrowRightAlt } from '@material-ui/icons'
 import Slider from '../../components/Slider'
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+    margin: 50px auto;
+    display: flex;
+    flex-direction: column;
+    width: 80vw;  
+
+    a {
+        color: #ef5f63;
+        margin-top: 20px;
+        font-family: 'Josefin Sans', sans-serif;
+        font-size: 1.3rem;
+        position: relative;
+        width: fit-content;
+
+
+        span {
+            vertical-align: middle;
+        svg {
+            transform: rotate(180deg);
+
+        }
+        }
+
+    }
+
+    a:after {    
+        background: none repeat scroll 0 0 transparent;
+        bottom: -5px;
+        content: "";
+        display: block;
+        height: 2px;
+        right: 0;
+        position: absolute;
+        background: #ef5f63;
+        transition: width 0.3s ease 0s, left 0.3s ease 0s;
+        width: 0;
+    }
+    a:hover:after { 
+        width: 100%; 
+        right: 0; 
+    }
+    
+`
+
+const Title = styled.div`
+
+    margin-bottom: 20px;
+    h1 {
+        margin: 0;
+        
+    }
+    p {
+        margin: 0;
+        span {
+            font-weight: bold;
+        }
+    }
+`
+
+const Body = styled.p`
+
+    position: relative;
+    width: 50vw;
+    max-width: 500px;
+    align-self: center;
+    text-align: justify;
+
+    span {
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+
+    .after:after {
+
+        content: "";
+        position: absolute;
+        right: -30vw;
+        left: -30vw;
+        height: 110%;
+        background-color: #00adb5;
+        z-index: -1;
+        opacity: 0.2;
+        bottom: 50%;
+        transform: translateY(50%);
+    }
+
+    p {
+        margin: 0;
+        
+        span {
+            font-weight: bolder;
+        }
+    }
+
+`
 
 
 const Project = ({ data }) => {
@@ -15,46 +112,6 @@ const Project = ({ data }) => {
         url: photo.file.url,
         title: photo.title
     }))
-
-    const slidesTest = [
-        {
-            url: "https://picsum.photos/id/1001/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/800/600",
-            title: "placeholder",
-        },
-        {
-            url: "https://picsum.photos/id/237/800/600",
-            title: "placeholder",
-        },
-
-    ]
 
     console.log(slides.length)
     return (
