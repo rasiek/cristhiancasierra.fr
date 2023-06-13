91951386d5d44f95a629c5267726a300ebfd1b8b
Cristhian <59307508+rasiek@users.noreply.github.com>
2023-06-13 07:33:33 +0200

    Delete src directory

--- src/components/Button.style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { Button } from '@material-ui/core'
-
-export const Wrapper = styled(Button)`
-    border-radius: 30px;
-    width: ${props => props.width || "50px"};
-`
-
